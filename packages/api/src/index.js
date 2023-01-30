4c4986d0b0633e4c12ce917921c2a04da042327e
lawson-ng <abraham.lawson.dev@gmail.com>
2023-01-30 20:32:07 +0700

    wip: config conveniet code

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -1,74 +1,72 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const app = express();
-const db = require("./db");
+const express = require("express")
+const bodyParser = require("body-parser")
+const morgan = require("morgan")
+const app = express()
+const db = require("./db")
 
-const PORT = 3001;
-const router = express.Router();
+const PORT = 3001
+const router = express.Router()
 
 // Middle ware
-app.use(bodyParser.json());
-app.use(helmet());
-app.use(morgan())
+app.use(bodyParser.json())
+app.use(morgan("dev"))
 
 router.post("/todo", (req, res) => {
-  console.log(req.body);
-  res.send("create new task");
+  console.log(req.body)
+  res.send("create new task")
 
-  const sql = "INSERT INTO todo (task) values (?)";
-  const params = [req.body?.task];
+  const sql = "INSERT INTO todo (task) values (?)"
+  const params = [req.body?.task]
 
   db.run(sql, params, function (error, result) {
-    console.log("error", error);
-    console.log("result", result);
-  });
-});
+    console.log("error", error)
+    console.log("result", result)
+  })
+})
 
 router.get("/todo", (req, res) => {
-  const sql = "SELECT * from todo";
-  const params = [];
+  const sql = "SELECT * from todo"
+  const params = []
 
   db.all(sql, params, (error, result) => {
     if (error) {
-      console.error(error);
+      console.error(error)
       res.json({
         status: "failed",
         error: "something went wrong!",
-      });
+      })
     } else {
       res.json({
         status: "success",
         data: result,
-      });
+      })
     }
-  });
-});
+  })
+})
 
 router.patch("/todo", (req, res) => {
-  res.send("update an existing task");
-});
+  res.send("update an existing task")
+})
 
 router.delete("/todo", (req, res) => {
-  const { id } = req.body;
-  const sql = "DELETE FROM todo WHERE id= ?";
+  const { id } = req.body
+  const sql = "DELETE FROM todo WHERE id= ?"
   db.run(sql, id, (error, result) => {
     if (error) {
       return res.json({
-        status: 'failed',
-        data: id
+        status: "failed",
+        data: id,
       })
     } else {
       return res.json({
-        status: 'success'
+        status: "success",
       })
     }
-  });
-});
+  })
+})
 
-app.use("/api", router);
+app.use("/api", router)
 
 app.listen(PORT, () => {
-  console.log("App listening on port", PORT);
-});
+  console.log("App listening on port", PORT)
+})
